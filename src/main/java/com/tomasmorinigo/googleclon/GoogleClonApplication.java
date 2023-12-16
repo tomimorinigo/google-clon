b3f03b49e24dc92b1f26210a6813bebcf4a51bce
tomiimorinigo <morinigotomas1@gmail.com>
2023-12-16 16:01:23 -0300

    Project initialized with maven and spring

---
+++ src/main/java/com/tomasmorinigo/googleclon/GoogleClonApplication.java
@@ -0,0 +1,13 @@
+package com.tomasmorinigo.googleclon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleClonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleClonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomasmorinigo/googleclon/GoogleClonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomasmorinigo.googleclon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleClonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
